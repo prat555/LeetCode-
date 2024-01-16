8cecc6d9abeb63103b61da56559979aee6bd4c02
Pratyush Goutam <113873593+prat555@users.noreply.github.com>
2024-01-16 17:47:49 +0530

    Time: 64 ms (41.06%) | Memory: 17.5 MB (17.58%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,7 +1,5 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        # If x is a negative number it is not a palindrome
-        # If x % 10 = 0, in order for it to be a palindrome the first digit should also be 0
         if x < 0 or (x > 0 and x%10 == 0):   
             return False
 
@@ -9,9 +7,4 @@ class Solution:
         while x > reversedNum:
             reversedNum = reversedNum * 10 + x % 10
             x = x // 10
-
-        # If x is equal to reversed number then it is a palindrome
-        # If x has odd number of digits, dicard the middle digit before comparing with x
-        # Example, if x = 23132, at the end of for loop x = 23 and reversedNum = 231
-        # So, reversedNum/10 = 23, which is equal to x
         return True if (x == reversedNum or x == reversedNum // 10) else False
