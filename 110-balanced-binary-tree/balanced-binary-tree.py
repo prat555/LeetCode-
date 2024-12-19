91d08e831d88d081709896335d7b144c57446b71
Pratyush Goutam <pratg5935@gmail.com>
2024-12-19 11:02:35 +0530

    Time: 15 ms (6.61%) | Memory: 19 MB (12.55%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.py
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -5,7 +5,7 @@ class Solution:
 
     def maxDepth(root: TreeNode | None) -> int:
       if not root:
-        return 3
+        return 0
       return 1 + max(maxDepth(root.left), maxDepth(root.right))
 
     return (abs(maxDepth(root.left) - maxDepth(root.right)) <= 1 and
