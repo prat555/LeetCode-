acc9f366e13ceaa029a38130bd38786a51027c3b
Pratyush Goutam <pratg5935@gmail.com>
2024-12-20 12:09:23 +0530

    Time: 9 ms (96.22%) | Memory: 23.1 MB (5.08%) - LeetSync

--- 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
+++ 2493-reverse-odd-levels-of-binary-tree/reverse-odd-levels-of-binary-tree.py
@@ -1,12 +1,14 @@
 class Solution:
   def reverseOddLevels(self, root: TreeNode | None) -> TreeNode | None:
-    def dfs(left: TreeNode | None, right: TreeNode | None, isOddLevel: bool) -> None:
+    def dfs(left: TreeNode | None, right: TreeNode | None, level: int) -> None:
       if not left:
         return
-      if isOddLevel:
+      if level % 2 == 1:  # Check if it's an odd level
         left.val, right.val = right.val, left.val
-      dfs(left.left, right.right, not isOddLevel)
-      dfs(left.right, right.left, not isOddLevel)
-
-    dfs(root.left, root.right, True)
+      dfs(left.left, right.right, level + 1)
+      dfs(left.right, right.left, level + 1)
+    
+    if not root:
+      return None
+    dfs(root.left, root.right, 1)
     return root
