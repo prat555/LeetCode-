11a793f893d5c26e2b693003860e9ee37548da7b
Pratyush Goutam <pratg5935@gmail.com>
2024-12-29 09:43:00 +0530

    Time: 622 ms (27.77%) | Memory: 0B (100.00%) - LeetSync

--- 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/employees-earning-more-than-their-managers.sql
@@ -2,4 +2,4 @@ SELECT Worker.name AS Employee
 FROM Employee AS Worker
 INNER JOIN Employee AS Manager
   ON (Worker.managerId = Manager.id)
-WHERE Worker.salary > Manager.salary
+WHERE Worker.salary > Manager.salary;
