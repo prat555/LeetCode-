fc4ba5086013bfb70ab050407ed0aa4525f3671f
Pratyush Goutam <pratg5935@gmail.com>
2025-01-02 17:06:05 +0530

    Time: 1401 ms (77.86%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT SalesPerson.name
+FROM Orders
+INNER JOIN Company
+  ON (Orders.com_id = Company.com_id AND Company.name = 'RED')
+RIGHT JOIN SalesPerson
+  USING (sales_id)
+WHERE Orders.sales_id IS NULL;
