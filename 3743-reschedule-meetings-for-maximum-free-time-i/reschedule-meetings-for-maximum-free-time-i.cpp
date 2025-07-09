c52f55c1f0d24dfd4af4cd7fd3d117e879f4532b
Pratyush Goutam <pratg5935@gmail.com>
2025-07-09 11:07:11 +0300

    Time: 0 ms (100.00%) | Memory: 123.4 MB (74.52%) - LeetSync

--- 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -1,24 +1,28 @@
 class Solution {
- public:
-  int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
-    const vector<int> gaps = getGaps(eventTime, startTime, endTime);
-    int windowSum = accumulate(gaps.begin(), gaps.begin() + k + 1, 0);
-    int ans = windowSum;
+public:
+    int maxFreeTime(int eventTime, int k, vector<int>& st, vector<int>& et) {
+         int n = st.size();
+        vector<int> gaps(n + 1);
 
-    for (int i = k + 1; i < gaps.size(); ++i) {
-      windowSum += gaps[i] - gaps[i - k - 1];
-      ans = max(ans, windowSum);
+        // Compute gaps
+        gaps[0] = st[0];  
+        gaps[n] = eventTime - et[n - 1];  
+        for(int i = 1; i < n; i++){
+            gaps[i] = st[i] - et[i - 1];
+        }
+
+        // Sliding window of size k + 1
+        int window = 0;
+        for(int i = 0; i <= k; i++){
+            window += gaps[i];
+        }
+
+        int ans = window;
+        for(int i = k + 1; i <= n; i++){
+            window += gaps[i] - gaps[i - (k + 1)];
+            ans = max(ans, window);
+        }
+
+        return ans;
     }
-
-    return ans;
-  }
-
- private:
-  vector<int> getGaps(int eventTime, const vector<int>& startTime, const vector<int>& endTime) {
-    vector<int> gaps{startTime[0]};
-    for (int i = 1; i < startTime.size(); ++i)
-      gaps.push_back(startTime[i] - endTime[i - 1]);
-    gaps.push_back(eventTime - endTime.back());
-    return gaps;
-  }
 };
