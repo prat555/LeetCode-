7849367981a1325e8430a3e181342a41bf4d10d6
Pratyush Goutam <pratg5935@gmail.com>
2025-08-06 18:31:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (95.59%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;      // Pointer for nums1's elements (excluding the extra space)
-        int j = n - 1;      // Pointer for nums2's elements
-        int k = m + n - 1;  // Pointer for the merged array's end
-        
-        // Merge in reverse order
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
+        int p1= m-1;
+        int p2= n-1;
+        int p= n+m-1;
+        while (p1>=0 && p2>=0){
+            if (nums1[p1]>nums2[p2]){
+                nums1[p]=nums1[p1];
+                --p1;
             }
+            else{
+                nums1[p]=nums2[p2];
+                --p2;
+            }
+            --p;
         }
-        
-        // If there are remaining elements in nums2, copy them
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
+        while (p2>=0){
+            nums1[p]=nums2[p2];
+            --p2;
+            --p;
         }
-        
-        // No need to handle remaining nums1 elements as they're already in place
     }
 };
