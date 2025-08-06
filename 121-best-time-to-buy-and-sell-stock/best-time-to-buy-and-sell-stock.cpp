823c53be4745a819b1837d16609d5a4568bca2d7
Pratyush Goutam <pratg5935@gmail.com>
2025-08-07 00:59:56 +0530

    Time: 0 ms (100.00%) | Memory: 97.5 MB (25.50%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -4,8 +4,8 @@ public:
         int min_price=prices[0];
         int mx_pr=0;
         for (int i=0; i<prices.size(); i++){
-            min_price=min(min_price,prices[i]);
             mx_pr=max(mx_pr,prices[i]-min_price);
+            min_price=min(min_price,prices[i]);
         } return mx_pr;
     }
 };
