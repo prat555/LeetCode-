71d3acac8056b8896a85e90b9a07d71f39a7cbf0
Pratyush Goutam <pratg5935@gmail.com>
2025-08-06 18:31:51 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (70.33%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -7,18 +7,18 @@ public:
         while (p1>=0 && p2>=0){
             if (nums1[p1]>nums2[p2]){
                 nums1[p]=nums1[p1];
-                --p1;
+                p1--;
             }
             else{
                 nums1[p]=nums2[p2];
-                --p2;
+                p2--;
             }
-            --p;
+            p--;
         }
         while (p2>=0){
             nums1[p]=nums2[p2];
-            --p2;
-            --p;
+            p2--;
+            p--;
         }
     }
 };
