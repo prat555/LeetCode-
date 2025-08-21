c994ef2ad221ca59f81199020d27b885b339ea55
Pratyush Goutam <pratg5935@gmail.com>
2025-08-21 08:15:20 +0530

    Time: 4 ms (88.33%) | Memory: 18.5 MB (68.58%) - LeetSync

--- 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
+++ 1628-count-submatrices-with-all-ones/count-submatrices-with-all-ones.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int numSubmat(vector<vector<int>>& mat) {
-        int rows = mat.size(), cols = mat[0].size();
-        vector<vector<int>> width(rows, vector<int>(cols, 0));
+        int m = mat.size(), n = mat[0].size();
+        vector<int> height(n, 0);
+        int ans = 0;
 
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (mat[row][col] == 1) {
-                    width[row][col] = col == 0 ? 1 : 1 + width[row][col - 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0)
+                    height[j] = 0;
+                else
+                    height[j] += 1;
+            }
+
+            for (int j = 0; j < n; j++) {
+                int min_h = height[j];
+                for (int k = j; k >= 0 && min_h > 0; k--) {
+                    min_h = min(min_h, height[k]);
+                    ans += min_h;
                 }
             }
         }
 
-        int totalCount = 0;
-
-        for (int topRow = 0; topRow < rows; ++topRow) {
-            for (int col = 0; col < cols; ++col) {
-                int minWidth = INT_MAX;
-                for (int bottomRow = topRow; bottomRow >= 0 && minWidth > 0; --bottomRow) {
-                    minWidth = min(minWidth, width[bottomRow][col]);
-                    totalCount += minWidth;
-                }
-            }
-        } 
-
-        return totalCount;
+        return ans;
     }
 };
