9460db748f3e1856f89de214c09f38f7b44833dc
Pratyush Goutam <pratg5935@gmail.com>
2025-10-21 23:34:39 +0530

    Time: 1375 ms (17.77%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,4 +1,2 @@
 # Write your MySQL query statement below
-select eu.unique_id,e.name 
-from employees e left join employeeUNI eu
-on e.id=eu.id;
+select eu.unique_id,e.name from employees e left join employeeUNI eu on e.id=eu.id;
