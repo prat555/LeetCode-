719eef800b23cdea8b520b8191e564dde405cbea
Pratyush Goutam <pratg5935@gmail.com>
2025-10-22 20:56:07 +0530

    Time: 321 ms (86.18%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -1,6 +1,5 @@
-SELECT
-  query_name,
-  ROUND(AVG(rating / position), 2) AS quality,
-  ROUND(AVG(rating < 3) * 100, 2) AS poor_query_percentage
-FROM Queries
-GROUP BY 1;
+select query_name, 
+round(avg(rating/position),2) as quality, 
+round(avg(rating<3)*100,2) as poor_query_percentage
+from queries
+group by 1
