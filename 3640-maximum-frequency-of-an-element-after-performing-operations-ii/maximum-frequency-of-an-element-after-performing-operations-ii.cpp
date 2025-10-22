760473a137e56ad3be0ba538219325069197bd50
Pratyush Goutam <pratg5935@gmail.com>
2025-10-22 06:34:59 +0530

    Time: 306 ms (70.14%) | Memory: 126 MB (71.56%) - LeetSync

--- 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -5,39 +5,24 @@ public:
         int ans = 0;
         map<int, int> count;
         
-        // Count frequency of each element
         for (int num : nums) {
             count[num]++;
-        }
-        
-        // Sort the array for sliding window approach
+        } 
         sort(nums.begin(), nums.end());
         
-        // Try each unique value as a potential target
         for (const auto& [target, freq] : count) {
-            // Elements already equal to target
             ans = max(ans, freq);
-            
-            // Find how many elements can be modified to become target
             int l = lower_bound(nums.begin(), nums.end(), target - k) - nums.begin();
-            int r = upper_bound(nums.begin(), nums.end(), target + k) - nums.begin() - 1;
-            
-            // Total elements in range [target-k, target+k]
+            int r = upper_bound(nums.begin(), nums.end(), target + k) - nums.begin() - 1;         
             int inRange = r - l + 1;
-            
-            // We can modify at most numOperations elements
-            // Result is original frequency + min(operations, elements that can be changed)
             ans = max(ans, min(freq + numOperations, inRange));
         }
         
-        // Try values not in the array using sliding window
         int j = 0;
         for (int i = 0; i < n; i++) {
-            // Expand window while elements are within 2*k range
             while (j < n && nums[j] - nums[i] <= 2 * k) {
                 j++;
             }
-            // Window size is j - i, we can pick at most numOperations elements
             ans = max(ans, min(j - i, numOperations));
         }
         
