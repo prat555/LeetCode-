a4a2f5697bb3123e87b55c8ba8e70311cb3b3dcc
Pratyush Goutam <pratg5935@gmail.com>
2025-10-22 21:09:38 +0530

    Time: 562 ms (63.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,3 @@
+select date_format(trans_date,"%Y-%m") as month, country, count(*) as trans_count,
+sum(state='approved') as approved_count, sum(amount) as trans_total_amount, sum(if(state='approved',amount,0)) as approved_total_amount
+from transactions group by 1,2 
