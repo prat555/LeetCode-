376826935afd6a69aad0379487ff129c57947514
Pratyush Goutam <pratg5935@gmail.com>
2025-11-05 16:07:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (11.22%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+ public:
+  int longestValidParentheses(string s) {
+    const string s2 = ")" + s;
+    vector<int> dp(s2.length());
+
+    for (int i = 1; i < s2.length(); ++i)
+      if (s2[i] == ')' && s2[i - dp[i - 1] - 1] == '(')
+        dp[i] = dp[i - 1] + dp[i - dp[i - 1] - 2] + 2;
+
+    return ranges::max(dp);
+  }
+};
