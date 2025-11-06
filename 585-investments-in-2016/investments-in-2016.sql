93ea08ab2a9e988fa317312fbdfe80137388714a
Pratyush Goutam <pratg5935@gmail.com>
2025-11-06 19:33:33 +0530

    Time: 520 ms (56.24%) | Memory: 0B (100.00%) - LeetSync

--- 585-investments-in-2016/investments-in-2016.sql
+++ 585-investments-in-2016/investments-in-2016.sql
@@ -1,16 +1,5 @@
-WITH DuplicateTIV AS (
-    SELECT tiv_2015
-    FROM Insurance
-    GROUP BY tiv_2015
-    HAVING COUNT(*) > 1
-),
-UniqueLocation AS (
-    SELECT lat, lon
-    FROM Insurance
-    GROUP BY lat, lon
-    HAVING COUNT(*) = 1
-)
-SELECT ROUND(SUM(i.tiv_2016), 2) AS tiv_2016
-FROM Insurance i
-INNER JOIN DuplicateTIV dt ON i.tiv_2015 = dt.tiv_2015
-INNER JOIN UniqueLocation ul ON i.lat = ul.lat AND i.lon = ul.lon;
+SELECT
+    ROUND(SUM(tiv_2016),2) AS tiv_2016
+FROM insurance
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM insurance GROUP BY tiv_2015 HAVING COUNT(*) > 1)
+AND (lat,lon) IN (SELECT lat,lon FROM insurance GROUP BY lat,lon HAVING COUNT(*) = 1)
