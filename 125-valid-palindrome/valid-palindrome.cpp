0f0ef3c341dd786403ca3241c7fb53a134f9972a
Pratyush Goutam <pratg5935@gmail.com>
2025-11-14 18:53:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (79.44%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l=0, r=s.length()-1;
+        while(l<r){
+            while (l<r && !isalnum(s[l])) l++;
+            while (l<r && !isalnum(s[r])) r--;
+            if (l<r){
+                if (tolower(s[l])!=tolower(s[r])) return false;
+                r--;
+                l++;
+            }
+        } return true;
+    }
+};
