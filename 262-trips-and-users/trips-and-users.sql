8dd39ec741a3b30fe69e55cf3dad760eae16410b
Pratyush Goutam <pratg5935@gmail.com>
2025-11-27 18:35:50 +0530

    Time: 515 ms (62.08%) | Memory: 0B (100.00%) - LeetSync

--- 262-trips-and-users/trips-and-users.sql
+++ 262-trips-and-users/trips-and-users.sql
@@ -7,6 +7,6 @@ INNER JOIN Users AS Clients
 INNER JOIN Users AS Drivers
   ON (Trips.driver_id = Drivers.users_id)
 WHERE Clients.banned = 'No'
-  AND Drivers.banned = 'No'
+  AND drivers.banned = 'No'
   AND Trips.request_at BETWEEN '2013-10-01' AND '2013-10-03'
 GROUP BY 1;
