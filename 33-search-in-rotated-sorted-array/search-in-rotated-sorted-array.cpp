74b7cf8e38c0fc57e51cff34a4c11eae5cfec65f
Pratyush Goutam <pratg5935@gmail.com>
2025-12-01 05:32:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (32.20%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -9,19 +9,17 @@ public:
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) return mid;
 
-            // If left half is sorted
             if (nums[left] <= nums[mid]) {
                 if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1; // target in left sorted half
+                    right = mid - 1;
                 } else {
-                    left = mid + 1;  // target in right half
+                    left = mid + 1;
                 }
             } else {
-                // Right half must be sorted
                 if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;  // target in right sorted half
+                    left = mid + 1;  
                 } else {
-                    right = mid - 1; // target in left half
+                    right = mid - 1; 
                 }
             }
         }
