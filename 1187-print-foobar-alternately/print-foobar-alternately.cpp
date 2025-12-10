72b818becd75780ebc83afeb41f4df212d09114f
Pratyush Goutam <pratg5935@gmail.com>
2025-12-10 21:23:26 +0530

    Time: 4 ms (83.95%) | Memory: 11.2 MB (92.55%) - LeetSync

---
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -0,0 +1,33 @@
+class FooBar {
+ public:
+  FooBar(int n) : n(n) {
+    sem_init(&fooSemaphore, /*pshared=*/0, /*value=*/1);
+    sem_init(&barSemaphore, /*pshared=*/0, /*value=*/0);
+  }
+
+  ~FooBar() {
+    sem_destroy(&fooSemaphore);
+    sem_destroy(&barSemaphore);
+  }
+
+  void foo(function<void()> printFoo) {
+    for (int i = 0; i < n; ++i) {
+      sem_wait(&fooSemaphore);
+      printFoo();
+      sem_post(&barSemaphore);
+    }
+  }
+
+  void bar(function<void()> printBar) {
+    for (int i = 0; i < n; ++i) {
+      sem_wait(&barSemaphore);
+      printBar();
+      sem_post(&fooSemaphore);
+    }
+  }
+
+ private:
+  const int n;
+  sem_t fooSemaphore;
+  sem_t barSemaphore;
+};
