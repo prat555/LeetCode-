b9b6c64145bd841d89b1b584d813240ba08b39d0
Pratyush Goutam <pratg5935@gmail.com>
2025-12-12 05:14:07 +0530

    Time: 3 ms (9.48%) | Memory: 9.1 MB (15.28%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> hashmap = {{')', '('}, {'}', '{'}, {']', '['}};
+        stack<char> stk;
+ 
+        for (char c : s) {
+            if (hashmap.find(c) == hashmap.end()) {
+                stk.push(c);
+            } else {
+                if (stk.empty() || stk.top() != hashmap[c]) {
+                    return false;
+                }
+                stk.pop();
+            }
+        }
+ 
+        return stk.empty();
+    }
+};
