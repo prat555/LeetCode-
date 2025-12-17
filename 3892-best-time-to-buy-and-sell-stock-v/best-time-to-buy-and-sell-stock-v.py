4a6a9a9e649efec8652bc80107283f439fb78119
Pratyush Goutam <pratg5935@gmail.com>
2025-12-17 12:18:23 +0530

    Time: 899 ms (95.12%) | Memory: 12.7 MB (60.98%) - LeetSync

---
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maximumProfit(self, prices, k):
+        """
+        :type prices: List[int]
+        :type k: int
+        :rtype: int
+        """
+        dp = [0]*(len(prices)+1)
+        result = 0
+        for i in xrange(k):
+            x, y = float("-inf"), float("-inf")
+            new_dp = [float("-inf")]*(len(prices)+1)
+            for j in xrange(i, len(prices)):
+                x, y = max(x, dp[j]-prices[j]), max(y, dp[j]+prices[j])
+                new_dp[j+1] = max(new_dp[j], x+prices[j], y-prices[j])
+            dp = new_dp
+            result = max(result, dp[-1])
+        return result
