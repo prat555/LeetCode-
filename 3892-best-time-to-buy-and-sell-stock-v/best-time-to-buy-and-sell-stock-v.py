74fbc57272eff910c96d74d27aea3b8940606c84
Pratyush Goutam <pratg5935@gmail.com>
2025-12-26 16:50:55 +0530

    Time: 965 ms (95.71%) | Memory: 12.7 MB (74.29%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.py
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.py
@@ -6,7 +6,7 @@ class Solution(object):
         :rtype: int
         """
         dp = [0]*(len(prices)+1)
-        result = 0
+        result= 0
         for i in xrange(k):
             x, y = float("-inf"), float("-inf")
             new_dp = [float("-inf")]*(len(prices)+1)
@@ -14,5 +14,5 @@ class Solution(object):
                 x, y = max(x, dp[j]-prices[j]), max(y, dp[j]+prices[j])
                 new_dp[j+1] = max(new_dp[j], x+prices[j], y-prices[j])
             dp = new_dp
-            result = max(result, dp[-1])
+            result =max(result, dp[-1])
         return result
