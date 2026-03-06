1d1ecfa8f557e1ea2ae59cc923df0c5b2eaaf82b
Pratyush Goutam <pratg5935@gmail.com>
2026-03-06 20:37:40 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (67.03%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        for (int i=0; i<nums.size(); i++){
-            if (nums[i]==target) return i;
+        int k, low, high;
+        int n=nums.size();
+        for (int i=0; i<n-1; i++){
+            if (nums[i]>nums[i+1]) k=i+1;
+        } if (target>=nums[k] && target<=nums[n-1]){
+            low=k;
+            high=n-1;
+        } else{
+            low=0;
+            high=k-1;
+        }
+        while (low<=high){
+            int mid=low+(high-low)/2;
+            if (nums[mid]==target) return mid;
+            else if (nums[mid]>target) high=mid-1;
+            else low=mid+1;
         } return -1;
     }
 };
