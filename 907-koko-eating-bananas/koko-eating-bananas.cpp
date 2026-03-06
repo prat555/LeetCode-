81cad8dc14129f12c923e30625d9ef27e24b7e9c
Pratyush Goutam <pratg5935@gmail.com>
2026-03-06 21:53:42 +0530

    Time: 12 ms (25.77%) | Memory: 22.9 MB (77.81%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,10 +1,5 @@
 class Solution {
 public:
-    long long itna(vector<int>& piles, int i){
-        long long ghanta=0;
-        for (int x:piles) ghanta+=(x+i-1)/i;
-        return ghanta;
-    }
     int minEatingSpeed(vector<int>& piles, int h) {
         int low=1, high=*max_element(piles.begin(),piles.end()), ans=high;
         while(low<=high){
@@ -14,5 +9,11 @@ public:
                 high=mid-1;
             } else low=mid+1;
         } return ans;
+    }
+private:
+    long long itna(vector<int>& piles, int i){
+        long long ghanta=0;
+        for (int x:piles) ghanta+=(x+i-1)/i;
+        return ghanta;
     }
 };
