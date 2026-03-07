e07a2fced2e765830a3286a5edcf329513ccf43b
Pratyush Goutam <pratg5935@gmail.com>
2026-03-07 13:10:08 +0530

    Time: 20 ms (55.20%) | Memory: 14.2 MB (93.60%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -1,24 +1,15 @@
 class Solution {
 public:
     int minFlips(string s) {
-        int n = s.size();
+        int n=s.size(), cnt=0;
         string target = "01";
-        int cnt = 0;
-        for (int i = 0; i < n; ++i) {
-            if (s[i] != target[i & 1]) {
-                ++cnt;
-            }
-        }
-        int ans = min(cnt, n - cnt);
-        for (int i = 0; i < n; ++i) {
-            if (s[i] != target[i & 1]) {
-                --cnt;
-            }
-            if (s[i] != target[(i + n) & 1]) {
-                ++cnt;
-            }
-            ans = min({ans, cnt, n - cnt});
-        }
-        return ans;
+        for (int i=0; i<n; ++i) {
+            if (s[i]!=target[i & 1]) ++cnt;
+        } int ans=min(cnt, n-cnt);
+        for (int i=0; i<n; ++i) {
+            if (s[i]!=target[i & 1]) --cnt;
+            if (s[i] != target[(i + n) & 1]) ++cnt;
+            ans=min({ans, cnt, n-cnt});
+        } return ans;
     }
 };
