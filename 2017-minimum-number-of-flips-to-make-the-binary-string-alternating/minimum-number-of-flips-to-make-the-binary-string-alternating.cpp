ee1f90c52108b52bc6f9c5fb08c350ef2268ab25
Pratyush Goutam <pratg5935@gmail.com>
2026-03-07 13:12:28 +0530

    Time: 31 ms (45.60%) | Memory: 14.3 MB (93.60%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -4,11 +4,11 @@ public:
         int n=s.size(), cnt=0;
         string target="01";
         for (int i=0; i<n; ++i) {
-            if (s[i]!=target[i & 1]) ++cnt;
+            if (s[i]!=target[i%2]) ++cnt;
         } int ans=min(cnt, n-cnt);
         for (int i=0; i<n; ++i) {
-            if (s[i]!=target[i & 1]) --cnt;
-            if (s[i] != target[(i + n) & 1]) ++cnt;
+            if (s[i]!=target[i%2]) --cnt;
+            if (s[i]!=target[(i+n)%2]) ++cnt;
             ans=min({ans, cnt, n-cnt});
         } return ans;
     }
