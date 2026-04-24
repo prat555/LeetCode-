5209b8e2df3c627f95fd97fedca63d6ab87dba5c
Pratyush Goutam <pratg5935@gmail.com>
2026-04-24 18:56:02 +0530

    Time: 87 ms (34.80%) | Memory: 88.8 MB (66.84%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,13 +4,12 @@ public:
         int mx=0;
         unordered_set<int> kk(nums.begin(),nums.end());
         for (int val:kk){
-            if (!kk.count(val-1)){
-                int cnt=0;
-                while (kk.count(val)){
-                    cnt++;
-                    val+=1;
-                } mx=max(cnt,mx);
-            }
+            if (kk.count(val-1)) continue;
+            int cnt=0;
+            while (kk.count(val)){
+                cnt++;
+                val+=1;
+            } mx=max(cnt,mx);
         } return mx;
     }
 };
