288239eb4b74baf8830511a1651ff4bf929e6e17
Pratyush Goutam <pratg5935@gmail.com>
2026-04-25 18:23:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (75.35%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -4,21 +4,17 @@ public:
         int k=1, le=0, ri=n-1, bot=n-1, top=0;
         vector<vector<int>> ans(n,vector<int>(n));
         while (k<=n*n){
-            for (int i=le; i<=ri; i++) 
-                ans[top][i]=k++;
-                top++;
-            if (top>bot) break;
-            for (int i=top; i<=bot; i++)
-                ans[i][ri]=k++;
-                ri--;
+            for (int i=le; i<=ri; i++) ans[top][i]=k++;
+            top++;
+            if (top>bot) return ans;
+            for (int i=top; i<=bot; i++) ans[i][ri]=k++;
+            ri--;
             if (k>n*n) return ans;
-            for (int i=ri; i>=le; i--)
-                ans[bot][i]=k++;
-                bot--;
+            for (int i=ri; i>=le; i--) ans[bot][i]=k++;
+            bot--;
             if (k>n*n) return ans;
-            for (int i=bot; i>=top; i--)
-                ans[i][le]=k++;
-                le++;
+            for (int i=bot; i>=top; i--) ans[i][le]=k++;
+            le++;
         } return ans;
     }
 };
