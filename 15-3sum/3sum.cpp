ebddb5834adf724f24fd33f855789f13d8f835b2
Pratyush Goutam <pratg5935@gmail.com>
2026-04-26 19:18:24 +0530

    Time: 39 ms (95.48%) | Memory: 29.2 MB (44.81%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int asize = nums.size();
-        vector<vector<int>> arr;
-        if (asize < 3) return arr;
-        sort(nums.begin(), nums.end());
-        
-        for (int i = 0; i < asize - 2; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-            if (nums[i] > 0) {
-                break;
-            }
-            
-            int left = i + 1;
-            int right = asize - 1;
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    arr.push_back({nums[i], nums[left], nums[right]});
-                    while (left < right && nums[left] == nums[left + 1]) {
-                        left++;
-                    }
-                    while (left < right && nums[right] == nums[right - 1]) {
-                        right--;
-                    }
-                    left++;
-                    right--;
-                } else if (sum < 0) {
-                    left++;
-                } else {
-                    right--;
+        vector<vector<int>> ans;
+        sort(nums.begin(),nums.end());
+        for (int i=0; i<nums.size(); i++){
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1, k=nums.size()-1;
+            while (j<k){
+                int num=nums[i]+nums[j]+nums[k];
+                if (num>0) k--;
+                else if (num<0) j++;
+                else{
+                    ans.push_back({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                    while (j<k && nums[j]==nums[j-1]) j++;
+                    while (j<k && nums[k]==nums[k+1]) k--;
                 }
             }
-        }
-        return arr;
+        } return ans;
     }
 };
