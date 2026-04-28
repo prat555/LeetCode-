51ca4f37ed7b6abcb99fba9609d8d4446c5bed41
Pratyush Goutam <pratg5935@gmail.com>
2026-04-28 18:55:46 +0530

    Time: 16 ms (84.77%) | Memory: 87 MB (78.60%) - LeetSync

--- 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
+++ 2160-minimum-operations-to-make-a-uni-value-grid/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int minOperations(vector<vector<int>>& grid, int x) {
-        vector<int> nums;
+        vector<int> flat;
         int mod=grid[0][0]%x;
         for (auto &row:grid){
             for (auto &val:row){
                 if (val%x!=mod) return -1;
-                nums.push_back(val/x);
+                flat.push_back(val/x);
             }
-        } nth_element(nums.begin(), nums.begin() + nums.size()/2, nums.end());
-        int med=nums[nums.size()/2];
+        } nth_element(flat.begin(), flat.begin() + flat.size()/2, flat.end());
+        int med=flat[flat.size()/2];
         int cnt=0;
-        for (int val:nums) cnt+=abs(val-med);
+        for (int val:flat) cnt+=abs(val-med);
         return cnt;
     }
 };
