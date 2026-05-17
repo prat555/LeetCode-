af26d8e24c1bc8d4f40f662c5ebc3ea4c8071956
Pratyush Goutam <pratg5935@gmail.com>
2026-05-17 15:06:00 +0530

    Time: 0 ms (100.00%) | Memory: 34.7 MB (96.68%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.cpp
+++ 1428-jump-game-iii/jump-game-iii.cpp
@@ -3,7 +3,7 @@ public:
     bool canReach(vector<int>& arr, int start) {
         if (start<0 || start>=arr.size() || arr[start]<0) return false;
         if (arr[start]==0) return true;
-        arr[start]=-arr[start];
+        arr[start] =-arr[start];
         return canReach(arr, start+arr[start]) || canReach(arr, start-arr[start]);
     }
 };
